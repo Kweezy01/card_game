2ac28fbc8ac0f1e6ee0a7e6fe00aa65d12476464
Kweezy <kewan3272001@gmail.com>
2022-08-16 20:24:06 +0200

    vercel issues

--- src/pages/Kreepz/Kreep_svgs.tsx
+++ src/pages/Kreepz/Kreep_svgs.tsx
@@ -20,7 +20,7 @@ const Bubbles = () => {
       <svg viewBox="0 0 78 73" fill="none" xmlns="http://www.w3.org/2000/svg">
          <g id="Group 2">
             <rect id="Rectangle 2" width="78" height="73" fill="#72E6FF" />
-            <ellipse id="Ellipse 4" rx="11" ry="10" transform="matrix(-1 0 0 1 54 13)" fill="#8BBAFF" fill-opacity="0.84" />
+            <ellipse id="Ellipse 4" rx="11" ry="10" transform="matrix(-1 0 0 1 54 13)" fill="#8BBAFF" fillOpacity="0.84" />
             <circle id="Ellipse 5" r="1.5" transform="matrix(-1 0 0 1 49.5 16.5)" fill="#D9D9D9" />
             <circle id="Ellipse 7" r="1.5" transform="matrix(-1 0 0 1 46.5 20.5)" fill="#FFA9E7" />
             <circle id="Ellipse 6" r="1.5" transform="matrix(-1 0 0 1 42.5 12.5)" fill="#D9D9D9" />
@@ -35,7 +35,7 @@ const Bubbles = () => {
             <path id="Ellipse 13" d="M40.5 23.5C40.5 24.6046 39.6046 25.5 38.5 25.5C37.3954 25.5 36.5 24.6046 36.5 23.5C36.5 22.3954 37.3954 21.5 38.5 21.5C39.6046 21.5 40.5 22.3954 40.5 23.5Z" fill="#05EB00" stroke="#900093" />
             <g id="Ellipse 14">
                <path d="M35 28C35 30.7614 32.7614 33 30 33C27.2386 33 25 30.7614 25 28C25 25.2386 27.2386 23 30 23C32.7614 23 35 25.2386 35 28Z" fill="#00FF38" />
-               <path d="M34.5 28C34.5 30.4853 32.4853 32.5 30 32.5C27.5147 32.5 25.5 30.4853 25.5 28C25.5 25.5147 27.5147 23.5 30 23.5C32.4853 23.5 34.5 25.5147 34.5 28Z" stroke="#8BBAFF" stroke-opacity="0.84" />
+               <path d="M34.5 28C34.5 30.4853 32.4853 32.5 30 32.5C27.5147 32.5 25.5 30.4853 25.5 28C25.5 25.5147 27.5147 23.5 30 23.5C32.4853 23.5 34.5 25.5147 34.5 28Z" stroke="#8BBAFF" strokeOpacity="0.84" />
             </g>
             <path id="Ellipse 15" d="M25.5 35.5C25.5 42.1274 20.1274 47.5 13.5 47.5C6.87258 47.5 1.5 42.1274 1.5 35.5C1.5 28.8726 6.87258 23.5 13.5 23.5C20.1274 23.5 25.5 28.8726 25.5 35.5Z" fill="#00FF38" stroke="#3C75AA" />
             <circle id="Ellipse 18" cx="21.5" cy="28.5" r="2.5" fill="#D9D9D9" />

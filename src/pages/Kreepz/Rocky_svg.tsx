bb8ae27822ecbabf2f43c09d4fbf003ada5f4e9d
Kweezy <kewan3272001@gmail.com>
2022-08-30 17:32:23 +0200

    Welcome Rocky!! (Using Switch instead of for loops as well)

---
+++ src/pages/Kreepz/Rocky_svg.tsx
@@ -0,0 +1,40 @@
+const Rocky = () => {
+    return(
+<svg width="175" height="175" viewBox="0 0 175 175" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g id="Rocky">
+<g id="Frame 1">
+<rect width="175" height="175" transform="matrix(-1 0 0 1 175 0)" fill="#4F0404"/>
+<path id="Vector 10" d="M80.3659 97.3394L76.4439 80.8788L86.2488 75L110.434 77.9394L97.361 97.3394L113.049 100.867L142.463 132.024L149 146.721L137.888 139.667L132.659 136.727L130.044 142.606L139.849 154.952H132.659L106.512 120.267L113.049 142.606V152.012L101.283 172L84.2878 152.012L65.3317 172L49.6439 152.012V142.606L60.1024 120.267L27.4195 154.952H20.8829L29.3805 146.721V142.606L22.8439 144.37L15 154.952L18.2683 142.606L60.1024 100.867L80.3659 97.3394Z" fill="#6C5246" stroke="black"/>
+<g id="Group 1">
+<ellipse id="Ellipse 26" rx="28.1154" ry="25.4091" transform="matrix(-1 0 0 1 113.731 35.5)" fill="#6D3B24"/>
+<ellipse id="Ellipse 27" rx="28.1154" ry="25.4091" transform="matrix(-1 0 0 1 86.2692 28.4091)" fill="#6D3B24"/>
+<ellipse id="Ellipse 28" rx="28.1154" ry="25.4091" transform="matrix(-1 0 0 1 111.115 62.4091)" fill="#6D3B24"/>
+<ellipse id="Ellipse 29" rx="28.1154" ry="25.4091" transform="matrix(-1 0 0 1 86.2692 56.7727)" fill="#6D3B24"/>
+<path id="Vector 11" d="M129 32C129 32 123.5 27.5 116.5 32" stroke="black"/>
+<path id="Vector 12" d="M86 27C86 27 80.5 22.5 73.5 27" stroke="black"/>
+</g>
+<rect id="Rectangle 5" width="13" height="12" transform="matrix(-1 0 0 1 128 35)" fill="#7B7B7B"/>
+<rect id="Rectangle 6" width="13" height="11" transform="matrix(-1 0 0 1 87 31)" fill="#7B7B7B"/>
+<ellipse id="Ellipse 30" rx="1.5" ry="2" transform="matrix(-1 0 0 1 78.5 39)" fill="#2F2F2F"/>
+<ellipse id="Ellipse 31" rx="2" ry="1.5" transform="matrix(-1 0 0 1 119 42.5)" fill="#2F2F2F"/>
+<g id="Group 2">
+<path id="Polygon 6" d="M102.615 73.9091L117.338 58.8409H87.893L102.615 73.9091Z" fill="#DC9170"/>
+<ellipse id="Ellipse 32" rx="3.26923" ry="2.95455" transform="matrix(-1 0 0 1 111.115 60.3182)" fill="#D9D9D9"/>
+<ellipse id="Ellipse 33" rx="3.26923" ry="2.95455" transform="matrix(-1 0 0 1 103.269 60.3182)" fill="#D9D9D9"/>
+<ellipse id="Ellipse 34" rx="3.26923" ry="2.95455" transform="matrix(-1 0 0 1 94.1154 60.3182)" fill="#D9D9D9"/>
+<rect id="Rectangle 7" width="41.8462" height="8.27273" transform="matrix(-1 0 0 1 122.231 52.6364)" fill="#6D3B24"/>
+</g>
+<path id="Vector 13" d="M49.5 152V142.5H112.5V152L101.5 172L84 152L65.5 172L49.5 152Z" fill="#914929" stroke="black"/>
+<path id="Vector 14" d="M70.5 166.5H60.5L65.5 172L70.5 166.5Z" fill="#4F1F0A" stroke="#1E0C03"/>
+<path id="Vector 15" d="M101.5 172L97.5 167.5H104.5L101.5 172Z" fill="#4F1F0A" stroke="#1E0C03"/>
+<path id="Vector 16" d="M100 52L80.5 61.5H84.5L100 55.5H107.5L115 61.5H121.5L107.5 52H100Z" fill="#973F17"/>
+<circle id="Ellipse 35" cx="73.5" cy="112.5" r="3.5" fill="#734150"/>
+<circle id="Ellipse 36" cx="94.5" cy="112.5" r="3.5" fill="#734150"/>
+<circle id="Ellipse 37" cx="84.5" cy="132.5" r="2.5" fill="#4E3C33"/>
+</g>
+</g>
+</svg>
+)
+}
+
+export default Rocky;
--- src/pages/Kreepz/Svg_router.tsx
+++ src/pages/Kreepz/Svg_router.tsx
@@ -1,17 +1,28 @@
 import Flamer from "./Flamer_svg"
 import Bubbles from "./Bubbles_svg"
 import Shadow from "./Shadow_svg"
+import Rocky from "./Rocky_svg"
 
 type svg_data = {
    name: string
 };
+
 const Svg_router = ({
    name
 }: svg_data) => {
-   if (name === "Flamer") return <Flamer />
-   else if (name === "Bubbles") return <Bubbles />
-   else if (name === "Shadow") return <Shadow />
-   return <h1 className="text-red-900" >No SVG with name `{name}` found</h1>
+   switch(name) {
+      case "Flamer": return <Flamer/>;
+      case "Bubbles": return <Bubbles/>;
+      case "Shadow": return <Shadow/>;
+      case "Rocky": return <Rocky/>;
+      case "Whisp": return <Whisp/>;
+      case "Bud-ly": return <Bud-ly/>;
+      case "Exo-bug": return <Exo-bug/>;
+      case "Hound": return <Hound/>;
+      case "Magnus": return <Magnus/>;
+      case "Nimbus": return <Nimbus/>;
+      default: return <h1 className="text-red-900" >No SVG with name `{name}` found</h1>;
+   }
 }
 
 export default Svg_router;
